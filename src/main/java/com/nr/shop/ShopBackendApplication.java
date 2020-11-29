7ffcb20dcad38330f0a6a04ae3e5777f2924b5c7
nikhil <nikhil rajnrj33@gmail.com>
2020-11-29 17:31:46 +0530

    Aws

--- src/main/java/com/nr/shop/ShopBackendApplication.java
+++ src/main/java/com/nr/shop/ShopBackendApplication.java
@@ -2,11 +2,12 @@ package com.nr.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ShopBackendApplication {
+public class ShopBackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopBackendApplication.class, args);
